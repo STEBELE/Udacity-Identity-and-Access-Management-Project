886757f66ec6c88a0ae2b685a2716cff0ebf8ff6
Sydney Tebele <kamogeloteb@gmail.com>
2022-08-26 05:51:16 +0200

    your comment

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-iaam.us.auth0.com', // the auth0 domain prefix
+    url: 'dev-iaam.us', // the auth0 domain prefix
     audience: 'coffee_shop', // the audience set for the auth0 app
     clientId: 'R6uiVxop2FcWFZ9Ok6996CCi5Q7TvTv2', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
